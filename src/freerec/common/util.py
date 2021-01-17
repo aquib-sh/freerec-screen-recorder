e0bf2433a3c1b7aa366b5723d199744e35f2d15e
Aquib <shaikhaquib394@gmail.com>
2021-01-17 18:47:19 +0530

    Add documentation comments

--- src/freerec/common/util.py
+++ src/freerec/common/util.py
@@ -1,6 +1,8 @@
 import os
 
 def mod_fname(basename, fformat):
+    """ If a file exists with same name then add 'n' 
+        to the end of new file. """
     if (basename + fformat) in os.listdir():
         basename += "n"
         return mod_fname(basename, fformat)
