034aa6fd52702493bd71247f4cf725aaa14ce2d2
Aquib <shaikhaquib394@gmail.com>
2021-01-17 17:39:32 +0530

    Change defaul FPS to 27

--- src/freerec/common/config.py
+++ src/freerec/common/config.py
@@ -12,14 +12,13 @@ app_icon       = os.path.join(resources, "icon_white.ico")
 app_name       = "FreeRec"
 window_size    = "400x300"
 video_encoding = "XVID"
-fps            =  30.0
+fps            =  27
 basename       = "Recording{}".format(
                             datetime.datetime.now().date()
                             )
 video_format   = ".avi"
 audio_format   = ".wav"
-video_fname    = mod_fname(basename, video_format)
-audio_fname    = mod_fname(basename, audio_format)
+output_path    = "Videos"
 resolution     = (GetSystemMetrics(0), GetSystemMetrics(1)) 
 
 
