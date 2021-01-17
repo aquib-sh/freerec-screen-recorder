4679179c51968f52c391aea496e0ea2f76d11063
Aquib <shaikhaquib394@gmail.com>
2021-01-17 14:48:15 +0530

    Add record_audio & merger_audio_and_video functions

--- src/freerec/resources/worker.py
+++ src/freerec/resources/worker.py
@@ -1,25 +1,34 @@
 import time
+import os
 import pyautogui
 import cv2
+import ffmpeg
 import numpy as np
 from freerec.common import config
 from freerec.common import util
+from freerec.resources.speech import audio_recog as sr
 
 class Worker():
     def __init__(self):
         self.codec = cv2.VideoWriter_fourcc(
             *config.video_encoding)
         self.toggle_stop = False
-    
-    def give_stop_signal(self):
+        self.r = sr.Recognizer()
+        self.r.pause_threshold=30
+        self.filenames = {'video':None, 'audio':None}
+        
+    def give_stop_signal(self, audio_status):
         self.toggle_stop = True
+        if audio_status == 1:
+            self.r.toggle_stop = True
 
     def record_video(self): 
         basename = config.basename
         format_  = config.video_format
+        self.filenames['video'] = util.mod_fname(basename, format_) 
 
         local_writer = cv2.VideoWriter(
-            filename  = util.mod_fname(basename, format_), 
+            filename = self.filenames['video'],
             fourcc    = self.codec,
             fps       = config.fps, 
             frameSize = config.resolution,
@@ -38,7 +47,43 @@ class Worker():
         self.toggle_stop = False
 
     def record_audio(self):
+        """ Records audio saves saves into file. """
         basename = config.basename
-        format_  = config.video_format
+        format_  = config.audio_format
+       
+        with sr.Microphone() as source:
+            audio = self.r.record(source)
+            
+        self.filenames['audio'] = util.mod_fname(basename, format_) 
+        with open(self.filenames['audio'], "wb") as f:
+            f.write(audio.get_wav_data())
+        print(self.filenames)
+        return
+
+    def merge_audio_and_video(self):
+        """ Merges audio and video files into one and 
+            then deletes the individual audio and video file. """
+        print("entered")
+        # If audio file was not recorded then there
+        # is no need to merge 
+        if self.filenames['audio'] == None:
+            print("none phase")
+            pass 
+        else:
+            video_file = ffmpeg.input(self.filenames['video'])
+            audio_file = ffmpeg.input(self.filenames['audio'])
+            print("entered now") 
+            temp = self.filenames['video'].split(".")
+            out_path = "F" + temp[0] + temp[1] 
+            print(out_path)
+            out = ffmpeg.output(
+                video_file, audio_file, 
+                out_path, vcodec='copy', 
+                acodec='aac', strict='experimental'
+                )
+            out.run(capture_stderr=True)
+            # Remove the intermediate audio file
+            os.remove(audio_file)
+        
 
 
